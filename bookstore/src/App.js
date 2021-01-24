1e2629e39c3d707b293de8a5075e4ebc316cf0c8
Mathura <mathuradas333@gmail.com>
2021-01-24 18:13:27 +0530

    [Mathura]Add. Added ability to fetch all data in app component using redux

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,4 +1,14 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { storeBookDetails } from "./redux";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(storeBookDetails());
+}, [dispatch])
+
   return (
     <div>
       Welcome to React Book Store
